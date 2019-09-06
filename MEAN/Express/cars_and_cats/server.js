cc65c0ee4848011321a4c3f62fd8b41a9f0138f9
anthony <anthonyb921@gmail.com>
2019-09-06 15:32:00 -0700

    added MEAN assignment

--- MEAN/Express/cars_and_cats/server.js
+++ MEAN/Express/cars_and_cats/server.js
@@ -13,6 +13,23 @@ app.get('/cats',(req,res) => {
 });
 app.get('/cars/new',(req,res) => {
     res.render('newcar');
-})
+});
+
+var cats = {
+    lion:{name:'lion',food:'lioness',age:3,sleep:['in the jungle, the mighty jungle','pride rock']},
+    tiger:{name:'tiger',food:'frosted flakes',age:5,sleep:['sometimes','never']},
+    burr:{name:'burr',food:'a smackerel of honey',age:7,sleep:['a cave','mr. sanders house']}   
+}
+
+app.get(`/lion`,(req,res) => {
+    res.render('details',{info:cats.lion});
+});
+app.get(`/tiger`,(req,res) => {
+    res.render('details',{info:cats.tiger});
+});
+app.get(`/burr`,(req,res) => {
+    res.render('details',{info:cats.burr});
+});
+
 
 app.listen(5000,() => console.log('listening on port 5000'));
